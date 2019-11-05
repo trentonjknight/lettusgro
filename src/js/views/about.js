9902be058dff8dab548d7f5d95cef599ca6d059c
audioryan9 <ryandsgn@outlook.com>
2019-11-05 16:12:49 +0000

    pushtues

--- src/js/views/about.js
+++ src/js/views/about.js
@@ -8,10 +8,10 @@ export const About = () => {
 		<>
 			<div className=" about, text-center pt-5 mt-5">
 				<h3 className="pgtitles">OUR MISSION</h3>
-				{/* <h5>
+				<h5 className="fontSize">
 					To inspire healthier communities by engaging people to harvest and benefit from multi-purpose
 					Microgreens!
-				</h5> */}
+				</h5>
 			</div>
 
 			<div className="card mb-3">
@@ -21,13 +21,20 @@ export const About = () => {
 					</div>
 					<div className="col-md-8">
 						<div className="card-body">
-							<h5 className="card-title">
-								To inspire healthier communities by engaging people to harvest and benefit from
-								multi-purpose Microgreens!
-							</h5>
-							<p className="card-text">
-								This is a wider card with supporting text below as a natural lead-in to additional
-								content. This content is a little bit longer.
+							<h5 className="card-title" />
+							<p className="CT, card-text">
+								Plan, sow, grow, harvest, market, and succeed! The Lettus encyclopedia easily allows for
+								careful growing, and propogating of your own vegetation without any of the
+								dissatisfaction that often follows traditional gardening. Here at Lettus Gro farm, we
+								{"'"}
+								ve combined our expert analysis, backed by scientific data, into one small web-based
+								resource, at your fingertips! but wait, where the real magic happens is; when automated
+								regulation mode is introduced with a little tech, in the form of a Raspberry Pi. Lettus
+								is a SMART garden monitoring system, complete with sensors for soil moisture, sunlight,
+								air quality, temperature, and humidity. The Raspberry can send out alerts directly to
+								the user, check to see if the ph is balanced in cases of hydrophonics cultivation, and
+								much much more! Notice that our mission is positioned to align with your values from
+								large businesses to small recreationalist alike -- so please Lettus grow with you :)
 							</p>
 							<p className="card-text">
 								<small className="text-muted">Last updated 3 mins ago</small>
