de79323d97c35777553b913a81646587fcca29f7
Trenton Knight <tknightj1995@yahoo.com>
2019-11-05 19:22:49 +0000

    building info page

--- src/js/views/cilantro.js
+++ src/js/views/cilantro.js
@@ -11,10 +11,41 @@ function Cilantro() {
 					<button className="btn btn-success text-light m-1">Mature Plant</button>
 				</div>
 			</div>
-			<div className="text-align-left">
-				<h5> Required Materials:</h5>
-				<p>For a scalable, mobile operation a 14in x 36in x 54in wire rack can house trays and lights.</p>
-				<p>16in x 11in x 13in plastic trays for germination.</p>
+			<div className="text-align-left m-2">
+				<ul>
+					{" "}
+					Required Materials:
+					<li>For a scalable, mobile operation a 14in x 36in x 54in wire rack will house trays and lights</li>
+					<li>16in x 11in x 13in plastic trays with matching cover for the germination period</li>
+					<li>13,000 Lumen 15in LED light strip</li>
+					<li>Coconut fiber pads(pre-cut) or roll (cut to size) </li>
+					<li>Zip ties to secure lighting to rack</li>
+					<li>Heating pad</li>
+					<li>Ventilation fans</li>
+					<li>Between 1lb-10lbs of seeds depending entirely on the desired operation size</li>
+					<li>Spray bottle</li>
+					<li>3% hydrogen peroxide</li>
+				</ul>
+			</div>
+			<div className="text-align-left m-2">
+				<ul>
+					Vital statistics from Trefle.io API
+					<li>Days to Maturity:</li>
+					<li>Precipitation Min - Max</li>
+					<li>Temperature Min - Max</li>
+					<li>Humidity Min - Max</li>
+					<li>Light Cycle </li>
+					<li>pH Min - Max</li>
+					<li>Palatable for Humans</li>
+					<li>Mature Height</li>
+				</ul>
+			</div>
+			<div className="text-align-left m-2">
+				<ol>
+					Grow instructions:
+					<li>Boil coconut fiber pads 10 mins on high.</li>
+					<li>Soak pads past boil for 24 hours.</li>
+				</ol>
 			</div>
 		</>
 	);
