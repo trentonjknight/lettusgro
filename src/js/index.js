0e0535c7f4c85a61267ff07efee660d04c8fcd0e
Trenton Knight <tknightj1995@yahoo.com>
2020-02-29 01:06:09 +0000

    history table

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 //include bootstrap npm library into the bundle
 // import "bootstrap/dist/css/bootstrap.css";
--- src/js/views/history.js
+++ src/js/views/history.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 //import PropTypes from "prop-types";
+import { Table } from "react-bootstrap";
 
 const History = props => {
 	return (
@@ -8,6 +9,35 @@ const History = props => {
 			<div className="text-center">
 				<h1>Grow Operation History</h1>
 			</div>
+			<Table striped bordered hover>
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Username</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>Mark</td>
+						<td>Otto</td>
+						<td>@mdo</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Jacob</td>
+						<td>Thornton</td>
+						<td>@fat</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td colSpan="2">Larry the Bird</td>
+						<td>@twitter</td>
+					</tr>
+				</tbody>
+			</Table>
 			<div className="mx-auto m-3">
 				<Link to="/history">
 					<button className="btn btn-success">Back Home</button>
